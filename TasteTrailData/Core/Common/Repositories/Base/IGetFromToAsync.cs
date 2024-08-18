ce09889e62f44413e79b5c12b7a6dfd8f524db2f
maksimkins <maks.aleshkov.04@gmail.com>
2024-08-18 21:58:22 +0400

    Change IEnumerable to List

--- TasteTrailData/Core/Common/Repositories/Base/IGetFromToAsync.cs
+++ TasteTrailData/Core/Common/Repositories/Base/IGetFromToAsync.cs
@@ -1,11 +1,6 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
 namespace TasteTrailData.Core.Common.Repositories.Base;
 
 public interface IGetFromToAsync<TEntity>
 {
-    Task<IEnumerable<TEntity>> GetFromToAsync(int from, int to);
+    Task<List<TEntity>> GetFromToAsync(int from, int to);
 }
