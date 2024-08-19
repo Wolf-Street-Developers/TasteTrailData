8a91bcfef0a59ea8b1921c52182167b041dd3a54
maksimkins <maks.aleshkov.04@gmail.com>
2024-08-19 16:17:22 +0400

    Change GetFroToAsync

--- TasteTrailData/Core/Common/Repositories/Base/IGetFromToAsync.cs
+++ TasteTrailData/Core/Common/Repositories/Base/IGetFromToAsync.cs
@@ -2,5 +2,5 @@ namespace TasteTrailData.Core.Common.Repositories.Base;
 
 public interface IGetFromToAsync<TEntity>
 {
-    Task<List<TEntity>> GetFromToAsync(int from, int to);
+    Task<List<TEntity>> GetFromToAsync(int from, int to, Predicate<TEntity>? filter = null);
 }
