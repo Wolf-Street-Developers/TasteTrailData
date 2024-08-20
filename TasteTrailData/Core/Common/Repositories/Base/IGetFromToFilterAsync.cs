0f90a1f472fc7b51e93a320f0b70362c151dc1be
maksimkins <maks.aleshkov.04@gmail.com>
2024-08-20 15:44:37 +0400

    Change IGetFromToFilterAsync

--- TasteTrailData/Core/Common/Repositories/Base/IGetFromToFilterAsync.cs
+++ TasteTrailData/Core/Common/Repositories/Base/IGetFromToFilterAsync.cs
@@ -1,6 +1,8 @@
+using System.Linq.Expressions;
+
 namespace TasteTrailData.Core.Common.Repositories.Base;
 
 public interface IGetFromToFilterAsync<TEntity>
 {
-    Task<List<TEntity>> GetFromToFilterAsync(int from, int to, Predicate<TEntity> filter);
+    Task<List<TEntity>> GetFromToFilterAsync(int from, int to, Expression<Func<TEntity, bool>> filter);
 }
