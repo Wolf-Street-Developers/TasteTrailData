d4df79256d17bea6b6b82c1fcfa0a6037de4da8c
maksimkins <maks.aleshkov.04@gmail.com>
2024-08-26 19:32:33 +0400

    Change IImageManager

--- TasteTrailData/Core/Common/Managers/ImageManagers/IImageManager.cs
+++ TasteTrailData/Core/Common/Managers/ImageManagers/IImageManager.cs
@@ -2,9 +2,9 @@ using Microsoft.AspNetCore.Http;
 
 namespace TasteTrailData.Core.Common.Managers.ImageManagers;
 
-public interface IImageManager<TId>
+public interface IImageManager<TId, TReturn>
 {
-    Task DeleteImage(TId id);
+    Task<string> DeleteImage(TId id);
 	Task<string> SetImage (TId id, IFormFile? logo);
 	Task<string> GetDefaultImageUrl();
 }
